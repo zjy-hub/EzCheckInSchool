f71679d66ac0c01d692a990f54f9db550723e70c
chillsoul <3213189@qq.com>
2020-09-13 21:54:06 +0800

    update

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ jsons = {
             "text": round(time.time() * 20)
         },
         "areaStr": {"streetNumber":"","street":"长椿路辅路","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"河南工业大学(莲花街校区)","lng":113.544407 + random.random()/10000,"lat":34.831014 + random.random()/10000,"address":"中原区长椿路辅路河南工业大学(莲花街校区)","text":"河南省-郑州市","code":""},
-        "reportdate": round(time.time() * 30),
+        "reportdate": round(time.time() * 1000),
         "customerid": 43,
         "deptid": deptid,
         "source": "app",
