6f2042dabb4acb3308f1dc621eb16efb8bcb6c7d
chillsoul <3213189@qq.com>
2020-09-14 12:40:42 +0800

    update the MOST EASY way

--- README.md
+++ README.md
@@ -19,25 +19,26 @@
 2020.9.12 7:00 修复时间判断代码，现在将正常打卡三次
 
 ## 使用方法
-首先，点击右上角`Star`并`Fork`
+首先，点击`Star`和`Fork`
+<button type="submit" class="btn btn-sm btn-with-count  js-toggler-target" aria-label="Unstar this repository" title="Star chillsoul/EzCheckInSchool" data-hydro-click="{&quot;event_type&quot;:&quot;repository.click&quot;,&quot;payload&quot;:{&quot;target&quot;:&quot;STAR_BUTTON&quot;,&quot;repository_id&quot;:294446353,&quot;originating_url&quot;:&quot;https://github.com/chillsoul/EzCheckInSchool&quot;,&quot;user_id&quot;:42226579}}" data-hydro-click-hmac="d76d13c9fe8c8f4dfc3124cd99a79b79adc1c00b33238e78926081d967cc450b" data-ga-click="Repository, click star button, action:files#disambiguate; text:Star">        <svg height="16" class="octicon octicon-star" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg> Star </button>
 
-接下来你需要把学号和姓名设为`Secert` 
+<button class="btn btn-sm btn-with-count" data-hydro-click="{&quot;event_type&quot;:&quot;repository.click&quot;,&quot;payload&quot;:{&quot;target&quot;:&quot;FORK_BUTTON&quot;,&quot;repository_id&quot;:294446353,&quot;originating_url&quot;:&quot;https://github.com/chillsoul/EzCheckInSchool&quot;,&quot;user_id&quot;:49368189}}" data-hydro-click-hmac="f8be48acbef26b397228e428d75ea61fea22ca140e88951bbbf4e851ac835fb7" data-ga-click="Repository, show fork modal, action:files#disambiguate; text:Fork" type="submit" title="Fork your own copy of chillsoul/EzCheckInSchool to your account" aria-label="Fork your own copy of chillsoul/EzCheckInSchool to your account">              <svg class="octicon octicon-repo-forked" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg> Fork </button>
+
+
+
+接下来你需要设置`Secert`
 
 你Fork的项目->Settings->Secert->New Secert
 
-Name:`DEPTID` Value:`班级ID ` 
+|Name|Value|示例|
+| :-----| :---- | :---- |
+|`DEPT_TEXT`|`学院班级专业`|`理学院-应用物理学-应物1901`|
+|`STU_ID`|`你的学号`|`201912340101`|
+|`STU_NAME`|`你的姓名`|`李华`|
+|`SC_URL`|[`Server酱调用URL`](http://sc.ftqq.com/?c=code)|`http://sc.ftqq.com/abcdefghigklmnopqrstuvwxyz.send`| 
 
-请在本项目[response.json](https://github.com/chillsoul/EzCheckInSchool/blob/master/response.json)中按Ctrl+F查询，如`计科F1901`的`DEPTID`为`227457`
+使用前请注册绑定[Server酱](http://sc.ftqq.com/)
 
-**重复班级请根据`parentId`和`打卡页面个人信息`判断属于什么学院、专业**
-
-Name:`DEPTTEXT` Value:`学院班级专业` 健康打卡界面个人信息下方 如`理学院-应用物理学-应物1901`
-
-Name:`STUNO` Value:`你的学号` 如`201812340101`
-
-Name:`USERNAME` Value:`你的姓名` 如`张三`
-
-Name:`SCKEY` Value:`Server酱调用URL` 使用前请绑定[Server酱](http://sc.ftqq.com/)，如` http://sc.ftqq.com/abcdefghigklmnopqrstuvwxyz.send`
 
 以上步骤完成后
 
@@ -47,8 +48,12 @@ Settings->Action->I understand... 开启Actions
 
 设置完成打卡后打卡时间内会每天自动打卡三次，第一次使用请观察效果。
 
-**注意：本项目默认学校为河南工业大学，其他学校请自行抓打卡提交的包（POST `https://reportedh5.17wanxiao.com/sass/api/epmpics` JSON）修改main.py代码。**
+**注意：本项目默认学校为河南工业大学，其他学校请自行抓包修改代码。**
 
+|Method|URL|修改|
+| :-----| :---- | :---- |
+|`POST`|`https://reportedh5.17wanxiao.com/sass/api/epmpics`|`main.py`|
+|`POST`|`https://reportedh5.17wanxiao.com/api/passcard/queryOrg`|`response.json`|
 ## 友情链接
 
 https://github.com/YooKing/HAUT_autoCheck - 校外版Python代码参考
--- main.py
+++ main.py
@@ -1,103 +1,154 @@
-import time
 import json
-import requests
+import time
 import random
-import datetime
+import requests
 
-# sectets字段录入
-deptid = input()
-depttext = input()
-stuNo = input()
-username = input()
-sckey = input()
+# 获取response.json dict
+with open('./response.json', 'r', encoding='utf8')as fp:
+    response_json = json.load(fp)
+    college_all = response_json['data']['collegeAll']
+    major_all = response_json['data']['majorAll']
+    class_all = response_json['data']['classAll']
 
-# 时间判断
-now = (time.localtime().tm_hour + 8) % 24
-if (now >= 6) & (now < 8):
-    templateid = "clockSign1"
-    customerAppTypeRuleId = 146
-elif (now >= 12) & (now < 14):
-    templateid = "clockSign2"
-    customerAppTypeRuleId = 147
-elif (now >= 21) & (now <= 22):
-    templateid = "clockSign3"
-    customerAppTypeRuleId = 148
-else:
-    print("现在是%d点%d分，将打卡早间档测试" %(now,time.localtime().tm_min))
-    templateid = "clockSign1"
-    customerAppTypeRuleId = 146
+# 输入Secrets
+stu_name = input()
+stu_id = input()
+dept_text = input()
+sc_url = input()
 
-# 随机温度(36.2~36.4)
-a = random.uniform(36.2, 36.4)
-temperature = round(a, 1)
 
-sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+def main():
+    """
+    主函数
+    """
+    # 获取学院、专业和班级信息
+    try:
+        info = dept_text.split('-', 3)
+        college_name = info[0]
+        major_name = info[1]
+        class_name = info[2]
+    except IndexError:
+        print_info_error()
+        exit(1)
 
-jsons = {
-    "businessType": "epmpics",
-    "method": "submitUpInfoSchool",
-    "jsonData": {
-        "deptStr": {
-            "deptid": deptid,
-            "text": depttext
-        },
-        "areaStr": {"streetNumber":"","street":"长椿路辅路","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"河南工业大学(莲花街校区)","lng":113.544407 + random.random()/10000,"lat":34.831014 + random.random()/10000,"address":"中原区长椿路辅路河南工业大学(莲花街校区)","text":"河南省-郑州市","code":""},
-        "reportdate": round(time.time() * 1000),
-        "customerid": 43,
-        "deptid": deptid,
-        "source": "app",
-        "templateid": templateid,
-        "stuNo": stuNo,
-        "username": username,
-        "userid": round(time.time() * 10),
-        "updatainfo": [
-            {
-                "propertyname": "temperature",
-                "value": temperature
+    # 获取deptId
+    try:
+        for college in college_all:
+            if college['name'] == college_name:
+                college_id = college['deptId']
+        for major in major_all:
+            if (major['name'] == major_name) & (major['parentId'] == college_id):
+                major_id = major['deptId']
+        for class_ in class_all:
+            if (class_['name'] == class_name) & (class_['parentId'] == major_id):
+                class_id = class_['deptId']
+    except NameError:
+        print_info_error()
+        exit(1)
+    # 时间判断 Github Actions采用国际标准时
+    time_h = (time.localtime().tm_hour + 8) % 24
+    time_m = time.localtime().tm_min
+    time_s = time.localtime().tm_sec
+    if (time_h >= 6) & (time_h < 8):
+        template_id = "clockSign1"
+        customer_app_type_rule_id = 146
+    elif (time_h >= 12) & (time_h < 14):
+        template_id = "clockSign2"
+        customer_app_type_rule_id = 147
+    elif (time_h >= 21) & (time_h <= 22):
+        template_id = "clockSign3"
+        customer_app_type_rule_id = 148
+    else:
+        print("现在是%d点%d分，将打卡早间档测试" % (time_h, time_m))
+        template_id = "clockSign1"
+        customer_app_type_rule_id = 146
+
+    # 随机温度(36.2~36.5)
+    a = random.uniform(36.2, 36.5)
+    temperature = round(a, 1)
+
+    check_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+
+    check_json = {
+        "businessType": "epmpics",
+        "method": "submitUpInfoSchool",
+        "jsonData": {
+            "deptStr": {
+                "deptid": class_id,
+                "text": dept_text
             },
-            {
-                "propertyname": "symptom",
-                "value": "无症状"
-            }
-        ],
-        "customerAppTypeRuleId": customerAppTypeRuleId,
-        "clockState": 0
-    },
-}
+            "areaStr": {"streetNumber": "", "street": "长椿路辅路", "district": "中原区", "city": "郑州市", "province": "河南省",
+                        "town": "", "pois": "河南工业大学(莲花街校区)", "lng": 113.544407 + random.random() / 10000,
+                        "lat": 34.831014 + random.random() / 10000, "address": "中原区长椿路辅路河南工业大学(莲花街校区)",
+                        "text": "河南省-郑州市", "code": ""},
+            "reportdate": round(time.time() * 1000),
+            "customerid": 43,
+            "deptid": class_id,
+            "source": "app",
+            "templateid": template_id,
+            "stuNo": stu_id,
+            "username": stu_name,
+            "userid": round(time.time()),
+            "updatainfo": [
+                {
+                    "propertyname": "temperature",
+                    "value": temperature
+                },
+                {
+                    "propertyname": "symptom",
+                    "value": "无症状"
+                }
+            ],
+            "customerAppTypeRuleId": customer_app_type_rule_id,
+            "clockState": 0
+        },
+    }
 
-# 提交打卡与结果判定
-flag = 0
-for i in range(1,5):
-    response = requests.post(sign_url, json=jsons)
-    utcTime = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
-    cstTime = utcTime.strftime("%H时%M分%S秒")
-    if response.status_code == 200:
-        flag=1
-        break
+    # 提交打卡与结果判定
+    flag = 0
+    for i in range(1, 5):
+        response = requests.post(check_url, json=check_json)
+        if response.status_code == 200:
+            flag = 1
+            break
+        else:
+            time.sleep(60)
+    print(response.text)
+    time_msg = str(time_h) + '时' + str(time_m) + '分' + str(time_s) + '秒'
+    if flag == 1:
+        if response.json()["msg"] == '成功':
+            msg = time_msg + '时' + "打卡成功"
+        else:
+            msg = time_msg + "打卡异常"
     else:
-        time.sleep(60)
-print(response.text)
-if flag == 1:
-    if response.json()["msg"] == '成功':
-        msg = cstTime + "打卡成功"
-    else:
-        msg = cstTime + "打卡异常"
-else:
-    msg = cstTime + "网络错误打卡失败"
+        msg = time_msg + "网络错误打卡失败"
 
-print(msg)
+    print(msg)
 
-# 微信通知
-title = msg
-result = json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
-content = f"""
-```
-{result}
-```
+    # 微信通知
+    title = msg
+    result = json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+    content = f"""
+    ```
+    {result}
+    ```
 
-"""
-data = {
-    "text": title,
-    "desp": content
-}
-req = requests.post(sckey, data=data)
+    """
+    data = {
+        "text": title,
+        "desp": content
+    }
+    requests.post(sc_url, data=data)
+
+
+def print_info_error():
+    """
+    打印 个人信息错误
+    """
+    print('请检查你填写的学院、专业、班级信息！')
+    print('见完美校园健康打卡页面')
+    print('如 理学院-应用物理学-应物1901')
+
+
+if __name__ == '__main__':
+    main()
